34cd8450bc42e2acb84ee76d81c3c2a48a588516
Lacey Trokey <trokeyln@gmail.com>
2023-09-21 18:12:12 -0600

    fixed bug with location, must find match of city and state

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
         var location = locationInputEl.val().toLowerCase()
         var [city, state] = location.split(',').map(str => str.trim())
 
-        var requestUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city},${state}&limit=1&appid=daf50ca1167039b8a5b4a47e15c528e8`
+        var requestUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city},${state}&limit=5&appid=daf50ca1167039b8a5b4a47e15c528e8`
 
         fetch(requestUrl)
             .then(function(response) {
@@ -15,15 +15,17 @@ $(document).ready(function() {
             .then(function(data) {
                 console.log('Fetch Response\n-------------')
                 console.log(data)
-                if (city == data.name && state == data.state) {
-                    console.log(data[0].name)
-                    console.log(data[0].state)
-                    console.log(data[0].lat)
-                    console.log(data[0].lon)
-                }
-                else {
-                    console.log('error')
-                }
-            });
+                var matchingLocation = data.find(function(location) {
+                    return location.name.toLowerCase() === city && location.state.toLowerCase() === state
+                  })
+
+                  if (matchingLocation) {
+                    console.log('Match found:')
+                    console.log(matchingLocation)
+                  } else {
+                    console.log('No matching location found.')
+                  }
+            })
+
     })
 })
