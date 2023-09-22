c9c83c483d947cea06d6ac6aeefb849abe7e7ce2
Lacey Trokey <trokeyln@gmail.com>
2023-09-22 11:11:22 -0600

    added press enter or click function to search

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,45 @@ $(document).ready(function() {
   var locationHistoryEl=$('#locationHistory')
   var locations = JSON.parse(localStorage.getItem('savedLocations')) || []
 
-  searchBtnEl.on('click', function() {
+  function saveLocation(matchingLocation) {
+    var existingLocation = locations.find(function(location) {
+      return (
+        location.name.toLowerCase() === matchingLocation.name.toLowerCase() &&
+        location.state.toLowerCase() === matchingLocation.state.toLowerCase()
+      )
+    })
+    if (!existingLocation) {
+      locations.push(matchingLocation)
+      localStorage.setItem('savedLocations', JSON.stringify(locations))
+      // displayLocation() - REMINDER BUG
+    }
+  }
+
+  function displayLocation() {
+    // locationHistoryEl.innerHTML = '' REMINDER BUG
+
+    for (var i = 0; i < locations.length; i++) {
+      var locationEntry = document.createElement('button')
+      locationEntry.textContent = locations[i].name + ', ' + locations[i].state
+      locationHistoryEl.append(locationEntry)
+    }
+  }
+
+  displayLocation()
+
+  searchBtnEl.on('click', searchLocation)
+
+  locationInputEl.on('keypress', function(event) {
+      if (event.key === 'Enter') {
+          searchLocation()
+      }
+  })
+
+  function searchLocation() {
     var location = locationInputEl.val().toLowerCase()
     var [city, state] = location.split(',').map(str => str.trim())
+
+    locationInputEl.val('')
 
     var requestUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city},${state}&limit=5&appid=daf50ca1167039b8a5b4a47e15c528e8`
 
@@ -19,31 +55,15 @@ $(document).ready(function() {
         console.log(data)
         var matchingLocation = data.find(function(location) {
           return location.name.toLowerCase() === city && location.state.toLowerCase() === state
-        })
-
+        });
         if (matchingLocation) {
           console.log('Match found:')
           console.log(matchingLocation)
           saveLocation(matchingLocation)
+          displayLocation();
         } else {
           console.log('No matching location found.')
         }
-        displayLocation()
       })
-    function saveLocation(matchingLocation) {
-      locations.push(matchingLocation)
-      localStorage.setItem('savedLocations',JSON.stringify(locations))
-    }
-    function displayLocation() {
-      locationHistoryEl.innerHTML = ''
-
-      for (var i = 0; i < locations.length; i++) {
-        var locationEntry = document.createElement('button')
-        locationEntry.textContent = locations[i].name + ', ' + locations[i].state
-        locationHistoryEl.append(locationEntry)
-      }
-    }
-
-
-  })
+  }
 })
