8093f3177b6ffb644df5cafa44d6d1f945fae95a
Lacey Trokey <trokeyln@gmail.com>
2023-09-25 18:07:54 -0600

    API updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
   const locations = JSON.parse(localStorage.getItem('savedLocations')) || []
-  var apiKey = window.location.hostname === 'https://ltrokey.github.io/weather_dashboard/' ? process.env.WEATHER_DASHBOARD_API_KEY : API_KEY
+  var apiKey = '0e85c09f82737f5cbeeaa614911739d4'
 
   // User Input & `Click`
  $('.searchBtn').on('click', function() {
